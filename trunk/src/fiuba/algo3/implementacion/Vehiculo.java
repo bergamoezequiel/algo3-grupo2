97827d057bc23331245b3cbd94be3da8f20603b9
bergamoezequiel@gmail.com <bergamoezequiel@gmail.com@fe8ef667-25e8-5bd9-8c86-7cd4ed781ff6>
2013-11-05 19:36:27 +0000

    Creada la clase Vehiculo

---
+++ trunk/src/fiuba/algo3/implementacion/Vehiculo.java
@@ -0,0 +1,15 @@
+package fiuba.algo3.implementacion;
+
+
+
+public class Vehiculo{
+	
+	private int movimientos;
+	public  Vehiculo(){
+		this.movimientos=0;
+	}
+	
+	public  int obtenerMovimientos(){
+		return movimientos;
+	}
+}
