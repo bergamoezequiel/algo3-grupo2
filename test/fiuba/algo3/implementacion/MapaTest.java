d8f1ebc769ef44d188e9a75d0eb0e8120e6542b5
RUITZEI@gmail.com <RUITZEI@gmail.com@fe8ef667-25e8-5bd9-8c86-7cd4ed781ff6>
2013-11-05 22:43:08 +0000

    Subida de la clase Mapa implementada con sus Respectivos Tests

--- test/fiuba/algo3/implementacion/MapaTest.java
+++ test/fiuba/algo3/implementacion/MapaTest.java
@@ -1,16 +1,40 @@
 package fiuba.algo3.implementacion;
 
-import junit.framework.Assert;
+import static org.junit.Assert.*;
+
 import org.junit.Test;
+
 
 public class MapaTest {
 
 	@Test
-	public void deberiaRetornarMapa(){
-        Mapa unMapa = new Mapa();
-        String result = unMapa.doMapa();
-        Assert.assertEquals("Mapa", result);
+	public void testDeberiaPoderCrearUnMapaConCiertoLarga(){
+		Mapa mapa = new Mapa(3,3);
+		assertEquals(3, mapa.getCantidadDeFilas());
+		assertEquals (3, mapa.getCantidadDeColumnas());
+	}
 	
+	@Test 
+	public void testDeberiaTenerTodasLasCeldasSinVehiculos(){
+		Mapa mapa = new Mapa (3,3);
+		for (int i = 0; i < mapa.getCantidadDeColumnas(); i++) {
+			for (int j = 0; j < mapa.getCantidadDeFilas(); j++) {
+				assertEquals (null, mapa.getCeldaEnPos(1,1).getVehiculo());
+			}			
+		}
+							
+	}
+	
+	@Test
+	public void testDeberiaPoderPonerUnVehiculoEnUnaCeldaDelMapa(){
+		Mapa mapa = new Mapa (3,3);
+		Vehiculo unVehiculo = new Vehiculo();
+		mapa.getCeldaEnPos(2, 1).setVehiculo(unVehiculo);
+		
+		assertEquals (unVehiculo , mapa.getCeldaEnPos(2 , 1).getVehiculo());
 	}
 
+	
+
+	
 }
