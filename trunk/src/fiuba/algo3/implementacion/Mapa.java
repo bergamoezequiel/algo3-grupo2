61578f33a258e67c9f12da95e8af32f4107a7892
RUITZEI@gmail.com <RUITZEI@gmail.com@fe8ef667-25e8-5bd9-8c86-7cd4ed781ff6>
2013-11-05 22:43:08 +0000

    Subida de la clase Mapa implementada con sus Respectivos Tests

--- trunk/src/fiuba/algo3/implementacion/Mapa.java
+++ trunk/src/fiuba/algo3/implementacion/Mapa.java
@@ -1,9 +1,45 @@
 package fiuba.algo3.implementacion;
 
-public class Mapa {
+import java.util.ArrayList;
 
-	public String doMapa(){
-		return "Mapa";
+public class Mapa {
+	
+	
+	private ArrayList<ArrayList<Celda>> celdas;
+	private int filas;
+	private int columnas;
+	
+	/*
+	 * OJO:  COLUMNAS x CELDAS son las dimensiones.
+	 * 
+	 */
+	public Mapa(int columnas, int filas) {
+		this.celdas = new ArrayList<ArrayList<Celda>>();
+		for (int i =0; i<columnas ; i++){
+			ArrayList<Celda> fila = new ArrayList<Celda>();
+			for (int j = 0; j < filas; j++){
+				fila.add(new Celda());
+			}
+			this.celdas.add(fila);
+			
+			this.columnas = columnas;
+			this.filas = filas;		
+		}
+		
 	}
 
+	public Celda getCeldaEnPos(int columna, int fila) {
+		ArrayList<Celda> col = this.celdas.get(columna);
+		return col.get(fila);
+	}
+		
+	
+	public int getCantidadDeFilas() {
+		return this.filas;
+	}
+	
+	public int getCantidadDeColumnas() {
+		return this.columnas;
+	}
+	
 }
