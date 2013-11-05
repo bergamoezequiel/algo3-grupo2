ff9c429e88b642c89f699ae88169837f8032ceb3
bergamoezequiel@gmail.com <bergamoezequiel@gmail.com@fe8ef667-25e8-5bd9-8c86-7cd4ed781ff6>
2013-11-05 19:36:27 +0000

    Creada la clase Vehiculo

---
+++ test/fiuba/algo3/implementacion/VehiculoTest.java
@@ -0,0 +1,15 @@
+package fiuba.algo3.implementacion;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class VehiculoTest {
+
+	@Test
+	public void vehiculoDeberiaTener0MovimientosAlCrearse(){
+        Vehiculo unVehiculo= new Vehiculo();
+        Assert.assertEquals(0, unVehiculo.obtenerMovimientos());
+	
+	}
+
+}
