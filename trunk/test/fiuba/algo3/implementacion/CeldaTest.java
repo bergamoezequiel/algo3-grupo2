1968148dbe294aaaf0e4e920dc6ba6cc400746c6
RUITZEI@gmail.com <RUITZEI@gmail.com@fe8ef667-25e8-5bd9-8c86-7cd4ed781ff6>
2013-11-05 21:28:19 +0000

    Implentada la clase Celda y sus respectivos Tests

--- trunk/test/fiuba/algo3/implementacion/CeldaTest.java
+++ trunk/test/fiuba/algo3/implementacion/CeldaTest.java
@@ -1,16 +1,26 @@
 package fiuba.algo3.implementacion;
 
-import junit.framework.Assert;
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
-public class CeldaTest {
+public class CeldaTest{
 
 	@Test
-	public void deberiaRetornarCelda(){
-        Celda unaCelda = new Celda();
-        String result = unaCelda.doCelda();
-        Assert.assertEquals("Celda", result);
+	public void testDeberiaInicializarceLaCeldaSinNingunVehiculo(){
+		Celda celda = new Celda();
+		assertEquals(null, celda.getVehiculo());	
+	}
 	
+	@Test
+	public void testDeberiaPoderPonerUnVehiculoEnUnaCelda(){
+		Vehiculo unVehiculo = new Vehiculo();
+		Celda celda = new Celda ();
+		
+		celda.setVehiculo( unVehiculo );
+		
+		assertEquals (unVehiculo, celda.getVehiculo());	
 	}
 
 }
+
