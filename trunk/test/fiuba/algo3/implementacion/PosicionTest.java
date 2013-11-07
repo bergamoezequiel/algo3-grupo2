6d5f46ad41a083ee8e53925d01945fca47597ecf
RUITZEI@gmail.com <RUITZEI@gmail.com@fe8ef667-25e8-5bd9-8c86-7cd4ed781ff6>
2013-11-07 03:23:05 +0000

    Refactoring de algunas Clases y agregados nuevos Tests.

--- trunk/test/fiuba/algo3/implementacion/PosicionTest.java
+++ trunk/test/fiuba/algo3/implementacion/PosicionTest.java
@@ -1,14 +1,29 @@
 package fiuba.algo3.implementacion;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Assert;
 import org.junit.Test;
 
+import fiuba.algo3.implementacion.Posicion;
+
 public class PosicionTest {
 	@Test
-	public void deberiaCrearUnaPosicionYDevolverSusParametrosCorrectamente(){
+	public void testDeberiaCrearUnaPosicionYDevolverSusParametrosCorrectamente(){
 		Posicion unaPosicion = new Posicion(2,1);
-        Assert.assertEquals(unaPosicion.getFila(), 2);
-        Assert.assertEquals(unaPosicion.getColumna(), 1);
+        assertEquals(unaPosicion.getFila(), 2);
+        assertEquals(unaPosicion.getColumna(), 1);
+	}
+	
+	@Test
+	public void testDeberiaPoderModificarUnaPosicion(){
+		Posicion unaPosicion = new Posicion (0,0);
+		unaPosicion.setColumna(2);
+		unaPosicion.setFila(2);
+		
+		assertEquals (unaPosicion.getColumna() , 2);
+		assertEquals (unaPosicion.getFila() , 2);
+		
 	}
 
 }
