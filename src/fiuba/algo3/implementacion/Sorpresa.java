0e367a1e0b4e33126d8a890e8019c8b5eaf1fdda
dipietromn@gmail.com <dipietromn@gmail.com@fe8ef667-25e8-5bd9-8c86-7cd4ed781ff6>
2013-11-09 22:18:04 +0000

    Se implementaron los metodos "dejarPasar" para cada vehiculo

--- src/fiuba/algo3/implementacion/Sorpresa.java
+++ src/fiuba/algo3/implementacion/Sorpresa.java
@@ -4,4 +4,11 @@ public abstract class Sorpresa extends ContenidoDeCelda implements Interactuable
 	public Sorpresa(){
 		super();
 	}
+	
+	public boolean dejaPasar(Moto unaMoto){ 
+		return true;}
+	public boolean dejaPasar(Auto unAuto){ 
+		return true;}
+	public boolean dejaPasar(TodoTerreno unTodoTerreno){ 
+		return true;}
 }
