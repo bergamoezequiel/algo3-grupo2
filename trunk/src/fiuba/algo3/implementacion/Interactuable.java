a85e596a7d127dd6b25c494b73c8fd63853ffffe
jmc.pippo@gmail.com <jmc.pippo@gmail.com@fe8ef667-25e8-5bd9-8c86-7cd4ed781ff6>
2013-11-09 23:53:25 +0000

    Creada la clase abstracta ObjetoEncontrable. Refactorizada clase Conductor

--- trunk/src/fiuba/algo3/implementacion/Interactuable.java
+++ trunk/src/fiuba/algo3/implementacion/Interactuable.java
@@ -1,9 +1,11 @@
 package fiuba.algo3.implementacion;
 
 public interface Interactuable {
+	//void interactuarCon(Conductor unConductor, Vehiculo unVehiculo);
 	void interactuarCon(Conductor unConductor, Moto unaMoto);
 	void interactuarCon(Conductor unConductor, Auto unAuto);
 	void interactuarCon(Conductor unConductor, TodoTerreno unTodoTerreno);
+	//boolean dejaPasar(Vehiculo unVehiculo);
 	boolean dejaPasar(Moto unaMoto);
 	boolean dejaPasar(Auto unAuto);
 	boolean dejaPasar(TodoTerreno unTodoTerreno);
