f829c9e34f37ce7988ee26c4a3acddeb212845c0
RUITZEI@gmail.com <RUITZEI@gmail.com@fe8ef667-25e8-5bd9-8c86-7cd4ed781ff6>
2013-11-09 22:16:10 +0000

    Modifico ConductorTest para agregar las nuevas Direcciones

--- test/fiuba/algo3/implementacion/ConductorTest.java
+++ test/fiuba/algo3/implementacion/ConductorTest.java
@@ -3,6 +3,7 @@ package fiuba.algo3.implementacion;
 import org.junit.Assert;
 import org.junit.Test;
 
+
 public class ConductorTest {
 
 	@Test
@@ -41,8 +42,24 @@ public class ConductorTest {
 		Conductor unConductor = new Conductor(new Auto());		
 		celdaInicial.agregarContenido(unConductor);
 		
+		//Avanza a la derecha.
 		Direccion derecha = new Derecha();
 		unConductor.avanzarEnDireccion(derecha);
-		Assert.assertEquals(celdaInicial.getVecino(derecha).getVecino(derecha).getCoordenada(), unConductor.getCelda().getCoordenada());
+		Assert.assertEquals(new Coordenada (2,0) , unConductor.getCelda().getCoordenada());
+
+		//Avanza para arriba.
+		Direccion arriba = new Arriba ();
+		unConductor.avanzarEnDireccion(arriba);
+		Assert.assertEquals(new Coordenada (2,2), unConductor.getCelda().getCoordenada());
+		
+		//Avanza para la izquierda.
+		Direccion izquierda = new Izquierda ();
+		unConductor.avanzarEnDireccion(izquierda);
+		Assert.assertEquals (new Coordenada(0, 2) , unConductor.getCelda().getCoordenada());
+		
+		//Avanza para Abajo.
+		Direccion abajo = new Abajo();
+		unConductor.avanzarEnDireccion(abajo);
+		Assert.assertEquals (new Coordenada(0, 0) , unConductor.getCelda().getCoordenada());
 	}
 }
