584f77407d09973e1f64be81b7e6ef3f6f84fb9f
RUITZEI@gmail.com <RUITZEI@gmail.com@fe8ef667-25e8-5bd9-8c86-7cd4ed781ff6>
2013-11-09 23:01:42 +0000

    Arreglo Build

--- trunk/src/fiuba/algo3/implementacion/CambioDeVehiculo.java
+++ trunk/src/fiuba/algo3/implementacion/CambioDeVehiculo.java
@@ -2,25 +2,46 @@ package fiuba.algo3.implementacion;
 
 public class CambioDeVehiculo extends Sorpresa {
 	
+
 	public CambioDeVehiculo() {
 		super();
 	}
-
-	public void InteractuarCon(Conductor unConductor, Moto unaMoto) {
+	
+	@Override
+	public void interactuarCon(Conductor unConductor, Moto unaMoto) {
 		Vehiculo unAuto = new Auto();
 		unConductor.cambiarDeVehiculo (unAuto);
 	}
 	
-	public void InteractuarCon(Conductor unConductor, Auto unAuto) {
+	@Override
+	public void interactuarCon(Conductor unConductor, Auto unAuto) {
 		Vehiculo unTodoTerreno = new TodoTerreno();
 		unConductor.cambiarDeVehiculo (unTodoTerreno);
 	}
 	
-	public void InteractuarCon(Conductor unConductor, TodoTerreno unTodoTerreno){
+	@Override
+	public void interactuarCon(Conductor unConductor, TodoTerreno unTodoTerreno){
 		Vehiculo unaMoto = new Moto();
 		unConductor.cambiarDeVehiculo (unaMoto);
        }
 
+
+	@Override
+	public boolean dejaPasar(Moto unaMoto) {
+		return true;
 	}
+
+
+	@Override
+	public boolean dejaPasar(Auto unAuto) {
+		return true;
+	}
+
+
+	@Override
+	public boolean dejaPasar(TodoTerreno unTodoTerreno) {
+		return true;
+	}
+}
 
 
