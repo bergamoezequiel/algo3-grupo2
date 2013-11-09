9818a9ab20d76ebb3db36a3282f577bb747fd390
bergamoezequiel@gmail.com <bergamoezequiel@gmail.com@fe8ef667-25e8-5bd9-8c86-7cd4ed781ff6>
2013-11-09 21:58:00 +0000

    Creada la Clase Piquete

---
+++ trunk/test/fiuba/algo3/implementacion/PiqueteTest.java
@@ -0,0 +1,54 @@
+package fiuba.algo3.implementacion;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PiqueteTest {
+	@Test
+	public void ElPiqueteTendriaQuePenalizarCon2MovimientosALasMotos(){
+        Piquete unPiquete= new Piquete();
+        Moto unaMoto=new Moto();
+        Conductor unConductor= new Conductor(unaMoto);
+        unPiquete.InteractuarCon(unConductor,unaMoto);
+        Assert.assertEquals(2,unConductor.getMovimientos());
+
+	}
+
+	@Test
+	public void ElPiqueteNoTendriaQuePenalizarALosAutos(){
+        Piquete unPiquete=new Piquete();
+        Auto unAuto=new Auto();
+        Conductor unConductor= new Conductor(unAuto);
+        unPiquete.InteractuarCon(unConductor,unAuto);
+        Assert.assertEquals(0,unConductor.getMovimientos());
+
+	}
+
+	@Test
+	public void ElPiqueteNoTendriaQuePenalizarALosTodoTerreno(){
+        Piquete unPiquete=new Piquete();
+        TodoTerreno unTodoTerreno=new TodoTerreno();
+        Conductor unConductor= new Conductor(unTodoTerreno);
+        unPiquete.InteractuarCon(unConductor,unTodoTerreno);
+        Assert.assertEquals(0,unConductor.getMovimientos());
+
+	}
+	public void ElPiqueteDeberiaDejarPasarALasMotos(){
+		Piquete unPiquete=new Piquete();
+		Moto unaMoto= new Moto();
+		Assert.assertEquals(true,unPiquete.dejaPasar(unaMoto));
+	}
+
+	public void ElPiqueteNoDeberiaDejarPasarALosAutos(){
+		Piquete unPiquete=new Piquete();
+		Auto unAuto= new Auto();
+		Assert.assertEquals(false,unPiquete.dejaPasar(unAuto));
+	}
+
+	public void ElPiqueteNoDeberiaDejarPasarALosTodoTerreno(){
+		Piquete unPiquete=new Piquete();
+		TodoTerreno unTodoTerreno=new TodoTerreno();
+		Assert.assertEquals(false,unPiquete.dejaPasar(unTodoTerreno));
+	}
+
+}
