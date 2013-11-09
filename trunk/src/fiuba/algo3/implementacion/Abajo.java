96aa316060e564ca0dd92c325a1946ad79fb75f7
RUITZEI@gmail.com <RUITZEI@gmail.com@fe8ef667-25e8-5bd9-8c86-7cd4ed781ff6>
2013-11-09 22:01:21 +0000

    Creadas las clases Arriba, Abajo e Izquierda

---
+++ trunk/src/fiuba/algo3/implementacion/Abajo.java
@@ -0,0 +1,10 @@
+package fiuba.algo3.implementacion;
+
+public class Abajo extends Direccion {
+	
+	public Abajo(){
+		super();
+		this.direccionX = 0;
+		this.direccionY = -1;
+	}
+}
---
+++ trunk/src/fiuba/algo3/implementacion/Arriba.java
@@ -0,0 +1,10 @@
+package fiuba.algo3.implementacion;
+
+public class Arriba extends Direccion {
+	
+	public Arriba(){
+		super();
+		this.direccionX = 0;
+		this.direccionY = 1;
+	}
+}
