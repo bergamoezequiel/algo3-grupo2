9114f7e0bb2fd1a10dcb0bc84f700780370d6263
bergamoezequiel@gmail.com <bergamoezequiel@gmail.com@fe8ef667-25e8-5bd9-8c86-7cd4ed781ff6>
2013-11-09 21:05:39 +0000

    Implementados metodos de la clase Pozo

--- trunk/src/fiuba/algo3/implementacion/Interactuable.java
+++ trunk/src/fiuba/algo3/implementacion/Interactuable.java
@@ -2,4 +2,5 @@ package fiuba.algo3.implementacion;
 
 public interface Interactuable {
 	void InteractuarCon(Conductor unConductor, Moto unaMoto);
+	void InteractuarCon(Conductor unConductor, Auto unAuto);
 }
