4c533eeb1e54e381e79181d5ed0db8dfb5a4410a
dipietromn@gmail.com <dipietromn@gmail.com@fe8ef667-25e8-5bd9-8c86-7cd4ed781ff6>
2013-11-10 22:37:09 +0000

    Se refactorizo la clase SorpresaDesfavorable y se agregaron Test correspondientes.

--- trunk/test/fiuba/algo3/implementacion/objetosEncontrables/SorpresaDesfavorableTest.java
+++ trunk/test/fiuba/algo3/implementacion/objetosEncontrables/SorpresaDesfavorableTest.java
@@ -3,30 +3,44 @@ package fiuba.algo3.implementacion.objetosEncontrables;
 import org.junit.Assert;
 import org.junit.Test;
 
-import fiuba.algo3.implementacion.objetosEncontrables.CambioDeVehiculo;
 import fiuba.algo3.implementacion.vehiculos.Auto;
+import fiuba.algo3.implementacion.vehiculos.Conductor;
 import fiuba.algo3.implementacion.vehiculos.Moto;
 import fiuba.algo3.implementacion.vehiculos.TodoTerreno;
 
 public class SorpresaDesfavorableTest {
 	@Test
-	public void LaSorpresaDesfavorableDeberiaDejarPasarALasMotos(){
+	public void testLaSorpresaDesfavorableDeberiaDejarPasarALasMotos(){
 		SorpresaDesfavorable unaSorpresaDesfavorable= new SorpresaDesfavorable();
 		Moto unaMoto= new Moto();
 		Assert.assertEquals(true,unaSorpresaDesfavorable.dejaPasar(unaMoto));
 	}
 	
 	@Test
-	public void LaSorpresaDesfavorableDeberiaDejarPasarALosAutos(){
+	public void testLaSorpresaDesfavorableDeberiaDejarPasarALosAutos(){
 		SorpresaDesfavorable unaSorpresaDesfavorable= new SorpresaDesfavorable();
 		Auto unAuto= new Auto();
 		Assert.assertEquals(true,unaSorpresaDesfavorable.dejaPasar(unAuto));
 	}
 	
 	@Test
-	public void LaSorpresaDesfavorableDeberiaDejarPasarALosTodoTerreno(){
+	public void testLaSorpresaDesfavorableDeberiaDejarPasarALosTodoTerreno(){
 		SorpresaDesfavorable unaSorpresaDesfavorable= new SorpresaDesfavorable();
 		TodoTerreno unTodoTerreno=new TodoTerreno();
 		Assert.assertEquals(true,unaSorpresaDesfavorable.dejaPasar(unTodoTerreno));
 	}
+	
+	@Test
+	public void testDeberiaAumentarLosMovimientosDeUnConductor(){
+		Auto unAuto = new Auto();
+		Conductor unConductor = new Conductor(unAuto);
+		SorpresaDesfavorable unaSorpresaDesfavorable = new SorpresaDesfavorable();
+		
+		unConductor.aumentarMovimientosEn(8);
+		
+		unaSorpresaDesfavorable.interactuarCon(unConductor, unAuto);
+		Assert.assertEquals(10, unConductor.getMovimientos());
+	}
+
+
 }
