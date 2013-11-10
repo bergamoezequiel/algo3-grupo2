9d4acfadf882f76552e42c0fcccc0b87024703a5
dipietromn@gmail.com <dipietromn@gmail.com@fe8ef667-25e8-5bd9-8c86-7cd4ed781ff6>
2013-11-10 21:51:33 +0000

    Se agregaron pruebas para el Cambio de Vehiculo

--- trunk/test/fiuba/algo3/implementacion/objetosEncontrables/CambioDeVehiculoTest.java
+++ trunk/test/fiuba/algo3/implementacion/objetosEncontrables/CambioDeVehiculoTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import fiuba.algo3.implementacion.objetosEncontrables.CambioDeVehiculo;
 import fiuba.algo3.implementacion.vehiculos.Auto;
+import fiuba.algo3.implementacion.vehiculos.Conductor;
 import fiuba.algo3.implementacion.vehiculos.Moto;
 import fiuba.algo3.implementacion.vehiculos.TodoTerreno;
 
@@ -29,4 +30,37 @@ public class CambioDeVehiculoTest {
 		TodoTerreno unTodoTerreno=new TodoTerreno();
 		Assert.assertEquals(true,unCambioDeVehiculo.dejaPasar(unTodoTerreno));
 	}
+	
+	@Test
+	public void testDeberiaCambiarAutoEnTodoTerreno(){
+		Auto unAuto = new Auto();
+		Conductor unConductor = new Conductor(unAuto);
+		TodoTerreno unTodoTerreno = new TodoTerreno();
+		CambioDeVehiculo unCambioDeVehiculo= new CambioDeVehiculo();
+		
+		unCambioDeVehiculo.interactuarCon(unConductor, unAuto);
+		Assert.assertEquals(unTodoTerreno, unConductor.getVehiculo());
+		}
+	
+	@Test
+	public void testDeberiaCambiarMotoEnAuto(){
+		Moto unaMoto = new Moto();
+		Conductor unConductor = new Conductor(unaMoto);
+		Auto unAuto = new Auto();
+		CambioDeVehiculo unCambioDeVehiculo= new CambioDeVehiculo();
+		
+		unCambioDeVehiculo.interactuarCon(unConductor, unaMoto);
+		Assert.assertEquals(unAuto, unConductor.getVehiculo());
+		}
+	
+	@Test
+	public void testDeberiaCambiarTodoTerrenoEnMoto(){
+		TodoTerreno unTodoTerreno = new TodoTerreno();
+		Conductor unConductor = new Conductor(unTodoTerreno);
+		Moto unaMoto = new Moto();
+		CambioDeVehiculo unCambioDeVehiculo= new CambioDeVehiculo();
+		
+		unCambioDeVehiculo.interactuarCon(unConductor, unTodoTerreno);
+		Assert.assertEquals(unaMoto, unConductor.getVehiculo());
+		}
 }
