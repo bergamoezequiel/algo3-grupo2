c56c40638b7c9da7ab225ce67efa6f3e87199048
jmc.pippo@gmail.com <jmc.pippo@gmail.com@fe8ef667-25e8-5bd9-8c86-7cd4ed781ff6>
2013-11-20 19:24:13 +0000

    Refactorizada la clase Ventana y AcercaDeVentana

--- src/fiuba/algo3/vista/AcercaDeVentana.java
+++ src/fiuba/algo3/vista/AcercaDeVentana.java
@@ -4,25 +4,25 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 
-public class AboutVentana extends JFrame {
+public class AcercaDeVentana extends JFrame {
 		        
      /*
 	 *   No se que es esta yerba pero si no se le agrega tira warning.
 	 */
 	private static final long serialVersionUID = 6576317272840853636L;
 
-		public AboutVentana(){
+		public AcercaDeVentana(){
 			setLocation(500, 100);
 			this.setVisible(false);                 
-			this.setTitle("About");
+			this.setTitle("Acerda de..");
 			this.setSize(300,300);
 			//this.setLocationRelativeTo(null);
 	                        
 			JLabel texto= new JLabel("Algoritmos y Programacion III , 2013."
-					+ "Integrantes: Juan Manuel Castro Pippo"
-					+         "   Manuel Ruiz Echazu"
-					+            " Mauro 'La Putita' DiPietro"
-					+	     "   Ezequiel 'El Fantasma' Bergamo.  		");
+					+ "\n Integrantes: Juan Manuel Castro Pippo"
+					+         "\n   Manuel Ruiz Echazu"
+					+            "\n Mauro 'La Putita' DiPietro"
+					+	     "\n   Ezequiel 'El Fantasma' Bergamo.  		");
 	                       
 			//texto.setHorizontalAlignment(SwingConstants.CENTER);
             //texto.setHorizontalTextPosition(SwingConstants.CENTER);
