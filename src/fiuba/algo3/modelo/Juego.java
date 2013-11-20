eb11c8c99c7da77251dd3180febd36ddcf3343b6
jmc.pippo@gmail.com <jmc.pippo@gmail.com@fe8ef667-25e8-5bd9-8c86-7cd4ed781ff6>
2013-11-20 18:38:21 +0000

    Creado metodo agregarObservador en la clase Juego.

--- src/fiuba/algo3/modelo/Juego.java
+++ src/fiuba/algo3/modelo/Juego.java
@@ -1,5 +1,6 @@
 package fiuba.algo3.modelo;
 
+import java.util.Observer;
 import fiuba.algo3.modelo.mapa.*;
 import fiuba.algo3.modelo.objetosEncontrables.*;
 import fiuba.algo3.modelo.vehiculos.*;
@@ -8,6 +9,7 @@ import fiuba.algo3.modelo.coordenadas.*;
 
 public class Juego {
 
+	private Observer observador;
 	private Mapa mapa;
 	private Conductor conductor;	
 	private int puntaje;
@@ -15,8 +17,11 @@ public class Juego {
 	public Juego (){
 		this.puntaje  = 0;
 		this.mapa= new Mapa (new Coordenada(11, 11));
-		this.conductor = new Conductor( new Auto());
-		
+		this.conductor = new Conductor(new Auto());	
+	}
+
+	public void agregarObservador(Observer unObservador){
+		this.observador = unObservador;
 	}
 	
 	public Mapa getMapa(){
