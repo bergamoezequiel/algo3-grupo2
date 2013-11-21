b919b0e187fafa820488f3fc01d50e5573d08a52
RUITZEI@gmail.com <RUITZEI@gmail.com@fe8ef667-25e8-5bd9-8c86-7cd4ed781ff6>
2013-11-21 03:33:57 +0000

    Modifique AcerDeVentana

--- src/fiuba/algo3/vista/AcercaDeVentana.java
+++ src/fiuba/algo3/vista/AcercaDeVentana.java
@@ -2,31 +2,25 @@ package fiuba.algo3.vista;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.SwingConstants;
+
 
 public class AcercaDeVentana extends JFrame {
 		        
      /*
 	 *   No se que es esta yerba pero si no se le agrega tira warning.
 	 */
-	private static final long serialVersionUID = 6576317272840853636L;
+	private static final long serialVersionUID = 1L;
 
 		public AcercaDeVentana(){
-			setLocation(500, 100);
+			setLocation(500, 200);
 			this.setVisible(false);                 
 			this.setTitle("Acerda de..");
-			this.setSize(300,300);
-			//this.setLocationRelativeTo(null);
+			this.setSize(300,100);
+			
 	                        
-			JLabel texto= new JLabel("Algoritmos y Programacion III , 2013."
-					+ "\n Integrantes: Juan Manuel Castro Pippo"
-					+         "\n   Manuel Ruiz Echazu"
-					+            "\n Mauro 'La Putita' DiPietro"
-					+	     "\n   Ezequiel 'El Fantasma' Bergamo.  		");
-	                       
-			//texto.setHorizontalAlignment(SwingConstants.CENTER);
-            //texto.setHorizontalTextPosition(SwingConstants.CENTER);
-            texto.setLocation(-10,100);
+			JLabel texto= new JLabel("          Algoritmos y Programacion III , 2013.");
+
+            texto.setLocation(100,100);
             texto.setSize(450,150);
             this.add(texto);              
                
