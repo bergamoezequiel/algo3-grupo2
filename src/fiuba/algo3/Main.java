a5626fae0028e828c2b7ec6a3bf7bc01ea409fd0
jmc.pippo@gmail.com <jmc.pippo@gmail.com@fe8ef667-25e8-5bd9-8c86-7cd4ed781ff6>
2013-11-23 23:12:19 +0000

    Volviendo a reimplementar el update que fallo antes.

--- src/fiuba/algo3/Main.java
+++ src/fiuba/algo3/Main.java
@@ -6,11 +6,11 @@ import fiuba.algo3.vista.Ventana;
 
 public class Main {
 	public static void main(String args[]){
-		Ventana unaVentana = new Ventana(); //Observador
+		//Ventana unaVentana = new Ventana(); //Observador
 		Juego unJuego = new Juego(); //Observado
 		//MainClass otraVentana = new MainClass();
 		
-		unJuego.addObserver(unaVentana);
+		//unJuego.addObserver(unaVentana);
 		/*observado.cambiarMensaje("Cambio 1");
 		observado.cambiarMensaje("Cambio 2");
 		observado.cambiarMensaje("Cambio 3");*/
