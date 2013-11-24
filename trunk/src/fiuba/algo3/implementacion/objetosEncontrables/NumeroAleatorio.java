de6ebd9b4493e3abb179e6d10a1961377c1821ce
bergamoezequiel@gmail.com <bergamoezequiel@gmail.com@fe8ef667-25e8-5bd9-8c86-7cd4ed781ff6>
2013-11-24 18:00:44 +0000

    Modificada la Clase ControlPolicial

---
+++ trunk/src/fiuba/algo3/implementacion/objetosEncontrables/NumeroAleatorio.java
@@ -0,0 +1,17 @@
+package fiuba.algo3.implementacion.objetosEncontrables;
+
+
+
+public class NumeroAleatorio {
+	
+	protected double numero;
+	
+	public NumeroAleatorio(){
+		this.numero= Math.random();
+	}
+	
+	public double obtenerNumero(){
+		return this.numero;
+	}
+	
+}
---
+++ trunk/src/fiuba/algo3/implementacion/objetosEncontrables/NumeroAleatorioFalso.java
@@ -0,0 +1,13 @@
+package fiuba.algo3.implementacion.objetosEncontrables;
+
+
+
+public class NumeroAleatorioFalso extends NumeroAleatorio{
+	
+	
+	public NumeroAleatorioFalso(double valor){
+		this.numero= valor;
+	}
+	
+	
+}
