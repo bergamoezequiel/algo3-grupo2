87a755210afe735a35d064c5286652b20eb0fc04
jmc.pippo@gmail.com <jmc.pippo@gmail.com@fe8ef667-25e8-5bd9-8c86-7cd4ed781ff6>
2013-11-25 22:05:05 +0000

    Agregada lista de Usuarios al juego.

--- trunk/src/fiuba/algo3/modelo/Juego.java
+++ trunk/src/fiuba/algo3/modelo/Juego.java
@@ -1,5 +1,6 @@
 package fiuba.algo3.modelo;
 
+import java.util.ArrayList;
 import java.util.Observable;
 
 import fiuba.algo3.modelo.mapa.*;
@@ -12,13 +13,18 @@ import fiuba.algo3.vista.Ventana;
 public class Juego extends Observable {
 
 	private Mapa mapa;
+	private ArrayList<Usuario> usuarios;
 	private Conductor conductor;    
 	private int puntaje;
 	private Pozo pozo;
 	private Piquete piquete;
 	
 	public Juego (){
-		this.mapa = new Mapa (12, 8);
+		this.usuarios = new ArrayList<Usuario>();
+		this.usuarios.add(new Usuario("Samus Aran"));
+		
+		this.puntaje = 0;
+		this.mapa = new Mapa (8, 8);
 		this.conductor = new Conductor(new Auto());     
 		this.pozo = new Pozo();
 		this.piquete= new Piquete();
@@ -27,7 +33,6 @@ public class Juego extends Observable {
 		this.mapa.getCeldaEn(new Coordenada(8,8)).agregarContenido(this.conductor);
 		this.mapa.getCeldaEn(new Coordenada(8,9)).agregarContenido(this.pozo);
 		this.mapa.getCeldaEn(new Coordenada(1,2)).agregarContenido(this.piquete);
-		this.puntaje = 0;
 	}
 
 	public Mapa getMapa(){
@@ -40,6 +45,10 @@ public class Juego extends Observable {
 	
 	public int getPuntaje(){
 		return this.puntaje;
+	}
+	
+	public void agregarUsuario(Usuario unUsuario){
+		this.usuarios.add(unUsuario);
 	}
 
 }
