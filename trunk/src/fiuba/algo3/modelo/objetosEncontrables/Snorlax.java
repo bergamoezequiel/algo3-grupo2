2213faecac9bb766e2b2bcaab28f82c41d8c9b5a
RUITZEI@gmail.com <RUITZEI@gmail.com@fe8ef667-25e8-5bd9-8c86-7cd4ed781ff6>
2013-11-28 23:14:25 +0000

    Creada la clase Snorlax ( pippo)

---
+++ trunk/src/fiuba/algo3/modelo/objetosEncontrables/Snorlax.java
@@ -0,0 +1,46 @@
+package fiuba.algo3.modelo.objetosEncontrables;
+
+import fiuba.algo3.modelo.vehiculos.Auto;
+import fiuba.algo3.modelo.vehiculos.Conductor;
+import fiuba.algo3.modelo.vehiculos.Moto;
+import fiuba.algo3.modelo.vehiculos.TodoTerreno;
+
+public class Snorlax extends ObjetoEncontrable{
+	int penalizacion;
+	
+	public Snorlax(){
+		super();
+		this.penalizacion = 0;
+	}
+
+	@Override
+	public void interactuarConAuto(Conductor unConductor) {	
+	}
+
+	@Override
+	public void interactuarConMoto(Conductor unConductor) {		
+	}
+
+	@Override
+	public void interactuarConTodoTerreno(Conductor unConductor) {
+		
+	}
+
+	@Override
+	public boolean dejaPasar(Auto unAuto) {
+		return false;
+	}
+
+	@Override
+	public boolean dejaPasar(Moto unaMoto) {
+		return false;
+	}
+
+	@Override
+	public boolean dejaPasar(TodoTerreno unTodoTerreno) {
+		return false;
+	}
+	
+	
+
+}
