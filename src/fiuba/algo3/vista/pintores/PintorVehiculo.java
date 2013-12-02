62a74acfd0e774c9fc640a187bbc1ffde5d809b5
jmc.pippo@gmail.com <jmc.pippo@gmail.com@fe8ef667-25e8-5bd9-8c86-7cd4ed781ff6>
2013-12-02 23:26:47 +0000

    Refactorizado utilizando tabla hash en la direccion de todos los pintores de vehiculos.

--- src/fiuba/algo3/vista/pintores/PintorVehiculo.java
+++ src/fiuba/algo3/vista/pintores/PintorVehiculo.java
@@ -1,15 +1,17 @@
 package fiuba.algo3.vista.pintores;
 
 import java.awt.Graphics;
+import java.util.Hashtable;
 
 import javax.swing.ImageIcon;
 
 import fiuba.algo3.modelo.coordenadas.*;
 
 public abstract class PintorVehiculo extends Pintor{
-
-	public void pintar(Graphics g, Coordenada unaCoordenadaVista, int anchoCelda, int altoCelda, Direccion unaDireccion){
 	
-	}
+	protected Hashtable<Class<?>, ImageIcon> indice;
+	
+	public void pintar(Graphics g, Coordenada unaCoordenadaVista, int anchoCelda, int altoCelda, Direccion unaDireccion){
+		}
 	
 }
