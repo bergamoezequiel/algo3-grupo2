b24335ae3998688c58ab81254d29a1e2ac7129c9
bergamoezequiel@gmail.com <bergamoezequiel@gmail.com@fe8ef667-25e8-5bd9-8c86-7cd4ed781ff6>
2013-12-03 02:51:47 +0000

    Niveles facil y vacio, persistidos por completo.

--- trunk/src/fiuba/algo3/Main.java
+++ trunk/src/fiuba/algo3/Main.java
@@ -8,46 +8,17 @@ import fiuba.algo3.vista.VentanaInicial;
 import fiuba.algo3.modelo.coordenadas.*;
 import fiuba.algo3.modelo.objetosEncontrables.*;
 import fiuba.algo3.modelo.vehiculos.*;
+import fiuba.algo3.modelo.*;
+
 public class Main {
 	public static void main(String args[]){
-/* CODIGO UTILIZADO PARA GUARDAR MAPAS EN ARCHIVOS XML
-		Mapa mapa = new Mapa (4, 4);
-		Conductor conductor = new Conductor(Moto.getInstancia(), new Derecha(), 6);
-		mapa.getCeldaEn(new Coordenada(7,6)).agregarContenido(new Llegada());
-		
-		
-		
-		mapa.getCeldaEn(new Coordenada(0,8)).agregarContenido(conductor);
-		//Snorlaxs
-				mapa.getCeldaEn(new Coordenada(0,1)).agregarContenido(new Snorlax());
-				mapa.getCeldaEn(new Coordenada(1,4)).agregarContenido(new Snorlax());
-				mapa.getCeldaEn(new Coordenada(6,1)).agregarContenido(new Snorlax());
-				mapa.getCeldaEn(new Coordenada(6,3)).agregarContenido(new Snorlax());
 
-				//Piquete
-				mapa.getCeldaEn(new Coordenada(5,0)).agregarContenido(new Piquete());
-			
-				//Cambios de Vehiculo
-				mapa.getCeldaEn(new Coordenada(3,0)).agregarContenido(new CambioDeVehiculo());
-				mapa.getCeldaEn(new Coordenada(0,5)).agregarContenido(new CambioDeVehiculo());
-				
-				//Controles Policiales
-				mapa.getCeldaEn(new Coordenada(3,2)).agregarContenido(new ControlPolicial());
-			
-				//Pozos
-				mapa.getCeldaEn(new Coordenada(1,8)).agregarContenido(new Pozo());
-				
-				//Sorpresas Desfavorables
-				mapa.getCeldaEn(new Coordenada(7,8)).agregarContenido(new SorpresaDesfavorable());
-				mapa.getCeldaEn(new Coordenada(5,6)).agregarContenido(new SorpresaDesfavorable());
-
-				//Sorpresa Favorables
-				mapa.getCeldaEn(new Coordenada(3,6)).agregarContenido(new SorpresaFavorable());
-		mapa.guardar();
-		*/
 		
 		Juego unJuego = new Juego(); //Observado
-		ControlPorTeclado unControl = new ControlPorTeclado(unJuego);
+		/* CODIGO PARA GENERAR LOS ARCHIVOS DE LOS NIVELES  http://www.youtube.com/watch?v=HWoVxBcvy1s
+		 NivelFacil nivel = new NivelFacil(Moto.getInstancia(), juego);
+		nivel.guardar();*/
+	ControlPorTeclado unControl = new ControlPorTeclado(unJuego);
 		
 		Ventana unaVentana = new Ventana(unJuego, unControl); //Observador
 		unJuego.addObserver(unaVentana);
--- trunk/src/fiuba/algo3/modelo/Nivel.java
+++ trunk/src/fiuba/algo3/modelo/Nivel.java
@@ -1,7 +1,14 @@
 package fiuba.algo3.modelo;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.Observable;
 import java.util.Observer;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
 
 import fiuba.algo3.modelo.coordenadas.Coordenada;
 import fiuba.algo3.modelo.mapa.Mapa;
@@ -64,4 +71,33 @@ public abstract class Nivel implements Observer {
 			this.juegoActual.conductorAlcanzoLaLlegadaDelNivelActual();
 		}
 	}
+	public Element serializar(){
+		Element nodoNivel= new Element("nivel");
+		nodoNivel.setAttribute("Nombre", this.getNombre());
+		nodoNivel.setAttribute("MovimientosLimites", Integer.toString(this.movimientosLimites));
+		nodoNivel.setAttribute("PuntajePorMovimientoSobrante", Integer.toString(this.puntajePorMovimientoSobrante));
+		nodoNivel.addContent(this.mapa.serializar());
+		return nodoNivel;
+		
+	}
+	
+
+	public void guardar(){
+		try{
+		Document docMapa =new Document();
+		System.out.println("Se creo el doc");
+		Element nivel = this.serializar();
+		
+		docMapa.setRootElement(nivel);
+		System.out.println("se agrego el mapa al documento");	
+		XMLOutputter xmlOutput =new XMLOutputter(Format.getPrettyFormat());
+		xmlOutput.output(docMapa,new FileOutputStream(new File("./src/archivos/nivelFacil.xml")));
+		System.out.println("Se escribio el archivo");
+		
+		}
+		catch(Exception ex){
+			ex.printStackTrace();
+		}
+}
+	
 }
