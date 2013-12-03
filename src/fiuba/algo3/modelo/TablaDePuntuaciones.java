048220ca17f70de4641a0a8dd918616cdb90b585
bergamoezequiel@gmail.com <bergamoezequiel@gmail.com@fe8ef667-25e8-5bd9-8c86-7cd4ed781ff6>
2013-12-03 03:20:57 +0000

    Persistencia de TablaDePuntuaciones

--- src/fiuba/algo3/modelo/TablaDePuntuaciones.java
+++ src/fiuba/algo3/modelo/TablaDePuntuaciones.java
@@ -1,7 +1,14 @@
 package fiuba.algo3.modelo;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.Collections;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
 
 import fiuba.algo3.modelo.mapa.Celda;
 
@@ -26,5 +33,33 @@ public class TablaDePuntuaciones {
 	public ElementoTablaDePuntuacion elementoEn(int unaPosicion){
 		return this.elementosTablaDePuntuacion.get(unaPosicion);
 	}
+	
+	public Element serializar(){
+		Element nodoTablaDePuntuacion = new Element("tabla");
+		for(int posicion=0;posicion<this.size();posicion++){
+		nodoTablaDePuntuacion.addContent(this.elementosTablaDePuntuacion.get(posicion).serializar());
+		}
+		return nodoTablaDePuntuacion;
+	}
+	
+
+	public void guardar(){
+		try{
+		Document docMapa =new Document();
+		System.out.println("Se creo el doc");
+		Element tabla = this.serializar();
+		
+		docMapa.setRootElement(tabla);
+		System.out.println("se agrego el mapa al documento");	
+		XMLOutputter xmlOutput =new XMLOutputter(Format.getPrettyFormat());
+		xmlOutput.output(docMapa,new FileOutputStream(new File("./src/archivos/tabla.xml")));
+		System.out.println("Se escribio el archivo");
+		
+		}
+		catch(Exception ex){
+			ex.printStackTrace();
+		}
+}
+	
 	
 }
