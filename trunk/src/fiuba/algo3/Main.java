26940992c63525489e8ba8afcb7fb22eec1c280e
bergamoezequiel@gmail.com <bergamoezequiel@gmail.com@fe8ef667-25e8-5bd9-8c86-7cd4ed781ff6>
2013-12-03 03:20:57 +0000

    Persistencia de TablaDePuntuaciones

--- trunk/src/fiuba/algo3/Main.java
+++ trunk/src/fiuba/algo3/Main.java
@@ -14,11 +14,26 @@ public class Main {
 	public static void main(String args[]){
 
 		
+	/*	CODIGO PARA GENERAR UN EJEMPLO DE LA TABLA
+	 * TablaDePuntuaciones tabla =new TablaDePuntuaciones();
+		Usuario usuario1 = new Usuario("juancito");
+		ElementoTablaDePuntuacion elemento1 = new ElementoTablaDePuntuacion(usuario1, 18);
+		tabla.agregar(elemento1);
+		Usuario usuario2= new Usuario("pepito");
+		ElementoTablaDePuntuacion elemento2 = new ElementoTablaDePuntuacion(usuario2, 25);
+		tabla.agregar(elemento2);
+		Usuario usuario3= new Usuario("lolita");
+		ElementoTablaDePuntuacion elemento3 = new ElementoTablaDePuntuacion(usuario3, 35);
+		tabla.agregar(elemento3);
+		tabla.guardar();*/
+		
+	
 		Juego unJuego = new Juego(); //Observado
 		/* CODIGO PARA GENERAR LOS ARCHIVOS DE LOS NIVELES  http://www.youtube.com/watch?v=HWoVxBcvy1s
 		 NivelFacil nivel = new NivelFacil(Moto.getInstancia(), juego);
 		nivel.guardar();*/
-	ControlPorTeclado unControl = new ControlPorTeclado(unJuego);
+
+		ControlPorTeclado unControl = new ControlPorTeclado(unJuego);
 		
 		Ventana unaVentana = new Ventana(unJuego, unControl); //Observador
 		unJuego.addObserver(unaVentana);
