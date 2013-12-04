95356c1e5da178b79afcf1b8f4da8c2f8331bb16
RUITZEI@gmail.com <RUITZEI@gmail.com@fe8ef667-25e8-5bd9-8c86-7cd4ed781ff6>
2013-12-04 16:39:51 +0000

    El juego verifica que no exista un usuario con ese mismo nombre en la ventana de Nuevo Usuario antes de crearlo.

--- test/fiuba/algo3/modelo/TestJuego.java
+++ test/fiuba/algo3/modelo/TestJuego.java
@@ -8,17 +8,31 @@ public class TestJuego {
 
 	@Test
 	public void testNoDeberiDejarPonerDosUsuariosConElMismoNombre(){
+		Juego unJuego = new Juego();
 		Usuario unUsuario = new Usuario("Juan");
 		Usuario otroUsuario = new Usuario("Mauro");
 		Usuario tercerUsuario = new Usuario("Juan");
-		Juego unJuego = new Juego();
 		
-		unJuego.agregarUsuario(unUsuario);
-		unJuego.agregarUsuario(otroUsuario);
-		unJuego.agregarUsuario(tercerUsuario);
+		
+		Assert.assertTrue(unJuego.agregarUsuario(unUsuario));
+		Assert.assertTrue(unJuego.agregarUsuario(otroUsuario));
+		Assert.assertFalse(unJuego.agregarUsuario(tercerUsuario));
 		
 		Assert.assertEquals(2, unJuego.getCantidadDeUsuarios());	
 	}
+	
+	@Test
+	public void deberiaAvisarmeSiSePudoCrearUnUsuarioConElNombreDado(){
+		Juego unJuego = new Juego();
+		Assert.assertTrue(unJuego.agregarUsuario(new Usuario("A")));
+		
+		//Agrego un usuario que no existía antes.
+		Assert.assertTrue( unJuego.agregarUsuario(new Usuario("Manuel")));
+		
+		//Intento agregar un usuario con un nombre que ya existe.
+		Assert.assertTrue( unJuego.agregarUsuario(new Usuario("Pedro")));
+	}
+	
 	
 
 }
