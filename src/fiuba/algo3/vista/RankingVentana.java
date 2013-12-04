b99c440cbe411edbc022f338b19571dc4bc567ca
RUITZEI@gmail.com <RUITZEI@gmail.com@fe8ef667-25e8-5bd9-8c86-7cd4ed781ff6>
2013-12-04 00:51:50 +0000

    Refactoring del Ranking para que sea vea mejor.

--- src/fiuba/algo3/vista/RankingVentana.java
+++ src/fiuba/algo3/vista/RankingVentana.java
@@ -1,5 +1,7 @@
 package fiuba.algo3.vista;
 
+import java.awt.Color;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
@@ -13,32 +15,46 @@ public class RankingVentana extends JFrame{
 	private static final long serialVersionUID = 1L;
 
 	public RankingVentana(){
-		setLocation(500, 200);
-		this.setVisible(false);                 
-		this.setTitle("Ranking");
-		this.setSize(250,200);
-		this.setResizable(false);
+		add(new PanelGPS());
 		TablaDePuntuaciones unaTabla = new TablaDePuntuaciones();
-		
 		unaTabla= TablaDePuntuaciones.leerXml();
+		
+
+		setLocation(500, 200);
+		this.setVisible(true);                 
+		this.setTitle("Ranking");
+		this.setSize(250,unaTabla.getTamanio()*100);
+		this.setResizable(true);
+	
+		
+		
 		                        
 		String str = "<html><div style='text-align: center;'>"
 				+ "<FONT FACE=verdana"
+				+ "<br>"
+				+ "<br>"
+				+ "<br>"
+				+ "<br>"
 				+ "<b>TAAAABLA DE POSICION JUAN! </b><br>"
 				+ "<br>"
-				+ "<br>";
+				+ "<TABLE BORDER WIDTH = '40%'>"
+				+ "<TR><TD> Nombre de Usuario </TD><TD> Puntaje</TD>";
 		
 				//Recorre la lista de usuarios.
-				for (int i = 0; i < unaTabla.getTamanio(); i++) {
-					str += unaTabla.getElementoEnPosicion(i).getNombre() + unaTabla.getElementoEnPosicion(i).getPuntaje() + "<br>";
+				for (int i = 0; i < unaTabla.getTamanio(); i++) {					
+					
+						str += "<TR><TD> " + unaTabla.getElementoEnPosicion(i).getNombre()+ "</TD><TD> " + unaTabla.getElementoEnPosicion(i).getPuntaje()+ " </TD>"
+						+ "</TR>";
 				};
 				
-				str += "<br>"
+				str +=  "</TABLE>" 
+				+ "<br>"
 				+ "<br>"		
 				+ "</html></div></font>";
 		
 		JLabel texto= new JLabel(str);
 		this.add(texto);
 		}
+
 	
 }
